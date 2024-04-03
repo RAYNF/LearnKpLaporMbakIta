29e2c4b7b3d1964017ef63f1f72d3956de8da8f3
Rezaafp <111202113571@mhs.dinus.ac.id>
2024-04-03 17:33:48 +0700

    update

---
+++ lapor_mbak_ita/lib/pages/login.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:lapor_mbak_ita/shared/theme_shared.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        bottom: false,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          children: [
+            Image.asset('assets/Logo.png', height: 300, fit: BoxFit.fill),
+            SizedBox(height: 51,),
+            Text("Welcome", style: primaryTextStyle, textAlign: TextAlign.center,),
+            SizedBox(height: 51,),
+            Container(
+              height: 60, width: MediaQuery.of(context).size.width - 2 * defaultMargin,
+              child: ElevatedButton(onPressed: (){},
+              style: ElevatedButton.styleFrom(backgroundColor: primaryColor, shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)
+              )), child: Text('Login', style: primaryTextStyle.copyWith(
+                fontSize: 16, fontWeight: FontWeight.w500, color: secondaryColor)))
+            ),
+            SizedBox(height: 15,),
+            Container(
+              height: 60, width: MediaQuery.of(context).size.width - 2 * defaultMargin,
+              child: ElevatedButton(onPressed: (){
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                  context: context, builder: (context){
+                    return StatefulBuilder(builder: (BuildContext context, StateSetter setState){
+                      return Wrap(
+                        children: [
+                          Container(
+                            color: Colors.transparent,
+                            child: Container(
+                              
+                              decoration: BoxDecoration(
+                                color: secondaryColor,
+                                borderRadius: BorderRadius.only(topRight: Radius.circular(40),
+                                topLeft: Radius.circular(40))
+                              ),
+                              child: Container(
+                                margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 25,),
+                                    Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                      Column(
+                                        children: [
+                                          Text("Hi!", style: primaryTextStyle.copyWith(fontSize: 20, color: secondaryColor),),
+                                          Text("Register", style: primaryTextStyle.copyWith(fontWeight: FontWeight.bold, fontSize: 30, color: secondaryColor),),
+                                        ],
+                                      ),
+                                      Spacer(),
+                                      Center(
+                                        child: InkWell(
+                                          onTap: (){
+                                            Navigator.pop(context);
+                                          },
+                                          child: Image.asset('assets/back.png', height: 30, width: 30,),
+                                        ),
+                                      ) 
+                                      
+                                    ],),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+                  });
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: primaryColor, shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)
+              )), child: Text('Register', style: primaryTextStyle.copyWith(
+                fontSize: 16, fontWeight: FontWeight.w500, color: secondaryColor)),)
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
---
+++ lapor_mbak_ita/lib/shared/theme_shared.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 24;
+
+Color primaryColor = Color(0x00D93A3A);
+Color secondaryColor = Color(0x00FFFFFF);
+Color darkColor = Color(0x001C274C);
+Color mutedColor = Color(0x00878787);
+Color verifikasiColor = Color(0x00F7DE00);
+Color selesaiColor = Color(0x005CB2F0);
+Color prosesColor = Color(0x004EF201);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  fontSize: 24, color: secondaryColor, fontWeight: FontWeight.w500
+);
